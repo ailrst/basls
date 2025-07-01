1af50a482a19de28b1877480cf93fa74b9e5c34a
am <alistair.michael@uq.edu.au>
2025-07-01 22:14:17 +1000

    reorganise files

--- ocamlBasil/bin/kwtypes.ml
+++
@@ -1,4 +0,0 @@
-type parsed =
-  | Keyword of ((int * int) * string)
-  | Comment of ((int * int) * string)
-[@@deriving show]
--- ocamlBasil/bin/util.ml
+++
@@ -1,3 +0,0 @@
-let compose f g x = f (g x)
-let get_or_else x default = match x with Some x -> x | None -> default ()
-let or_else default x = match x with Some x -> x | None -> default ()
